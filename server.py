cedd2f80217531d06b520b2ea225ffc69f0836db
Alex Camilleri <camilleri.alex@gmail.com>
2024-10-27 02:39:29 +0200

    Attempt to wide login. Click works, enter doesn't

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ from analyze import (
     get_following_lists,
     get_mastodon_api,
     get_user_from_handle,
+    parse_mastodon_handle,
 )
 
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -46,25 +47,24 @@ app.config["MASTODON_CLIENT_SECRET"] = CLIENT_SECRET
 app.config["MASTODON_INSTANCE"] = INSTANCE
 
 oauth = OAuth(app)
-oauth.register(
-    name="mastodon",
-    api_base_url="https://"+INSTANCE+"/api/v1",
-    access_token_url="https://"+INSTANCE+"/oauth/token",
-    authorize_url="https://"+INSTANCE+"/oauth/authorize",
-    client_id=CLIENT_KEY,
-    client_secret=CLIENT_SECRET,
-    client_kwargs={
-        "scope": "read",
-    },
-    fetch_token=lambda: session.get("token"),  # DON'T DO IT IN PRODUCTION
-)
-
-mastodon = oauth.mastodon
-
 
 @app.route("/login")
 def login():
-    redirect_uri = url_for("oauth_authorized", _external=True)
+    instance = request.args.get('instance')
+    oauth.register(
+        name="mastodon",
+        api_base_url="https://"+instance+"/api/v1",
+        access_token_url="https://"+instance+"/oauth/token",
+        authorize_url="https://"+instance+"/oauth/authorize",
+        client_id=CLIENT_KEY,
+        client_secret=CLIENT_SECRET,
+        client_kwargs={
+            "scope": "read",
+        },
+        fetch_token=lambda: session.get("token"),  # DON'T DO IT IN PRODUCTION
+    )
+
+    redirect_uri = url_for("oauth_authorized", instance=instance, _external=True)
     return oauth.mastodon.authorize_redirect(redirect_uri)
 
 
@@ -84,8 +84,9 @@ def handle_error(error):
 
 @app.route("/authorized")
 def oauth_authorized():
+    instance = request.args.get('instance')
     token = oauth.mastodon.authorize_access_token()
-    resp = oauth.mastodon.get(f"https://{INSTANCE}/api/v1/accounts/verify_credentials")
+    resp = oauth.mastodon.get(f"https://{instance}/api/v1/accounts/verify_credentials")
     print("Response status:", resp.status_code)
     print("Response text:", resp.text)
 
@@ -98,7 +99,7 @@ def oauth_authorized():
         
     session["mastodon_token"] = token["access_token"]
     session["mastodon_user"] = profile["acct"]
-    instance="mastodon.social" # TODO: change
+
     try:
         session["lists"] = get_following_lists(
             profile["id"],
@@ -112,9 +113,11 @@ def oauth_authorized():
     flash("You were signed in as %s" % profile["display_name"])
     return redirect("/")
 
+class LoginForm(Form):
+    login_acct = StringField("Mastodon user handle")
 
 class AnalyzeForm(Form):
-    acct = StringField("Mastodon User")
+    analyze_acct = StringField("Mastodon user handle")
     lst = SelectField("List")
 
 
@@ -122,82 +125,109 @@ class AnalyzeForm(Form):
 def index():
     tok = session.get(
         "mastodon_token")
-    form = AnalyzeForm(request.form)
-    if session.get("lists"):
-        form.lst.choices = [("none", "No list")] + [
-            (str(list["id"]), list["name"]) for list in session["lists"]
-        ]
-    else:
-        del form.lst
 
-    different_user = form.acct.data != session.get("mastodon_user")
+    form = LoginForm(request.form)
+    
+    if request.method == "POST":
+        form_type = request.form.get("form_type")
+        if form_type == "login":
+            form = LoginForm(request.form)
+            handle = form.login_acct.data
+            username, instance = parse_mastodon_handle(handle)
+            
+            return render_template(
+                "index.html",
+                form=form,
+                div=div,
+                TRACKING_ID=TRACKING_ID,
+            )
+        elif form_type == "analyze":
+            form = AnalyzeForm(request.form)
 
-    results = {}
-    list_name = list_id = error = None
-    if request.method == "POST" and form.validate() and form.acct.data:
-        # Don't show auth'ed user's lists in results for another user.
-        if hasattr(form, "lst") and different_user:
-            del form.lst
+            if session.get("lists"):
+                form.lst.choices = [("none", "No list")] + [
+                    (str(list["id"]), list["name"]) for list in session["lists"]
+                ]
+            else:
+                del form.lst
 
-        if app.config["DRY_RUN"]:
-            list_name = None
-            following, followers, timeline = dry_run_analysis()
-            results = {"following": following, "followers": followers, "timeline": timeline}
-        else:
-            if session.get("lists") and form.lst and form.lst.data != "none":
-                list_id = int(form.lst.data)
-                list_name = [
-                    list["name"] 
-                    for list in session["lists"] 
-                    if int(list["id"]) == list_id
-                ][0]
+            different_user = form.analyze_acct.data != session.get("mastodon_user")
 
-            try:
-                api = get_mastodon_api(
-                    tok, INSTANCE
-                )
-                cache = Cache()
+            results = {}
+            list_name = list_id = error = None
+    
+            if form.validate() and form.analyze_acct.data:
+            # Don't show auth'ed user's lists in results for another user.
+                if hasattr(form, "lst") and different_user:
+                    del form.lst
 
-                user = get_user_from_handle(form.acct.data, api)
-
-                if different_user and user.indexable is False:
-                    print("YOOOOOOOOOO")
-                    raise Exception(
-                        f"User {form.acct.data} is not indexable.\n"
-                        f"If the account is yours, you can change the setting on: "
-                        f"Settings > Public profile > Privacy and reach > "
-                        f"Include public posts in search results"
-                    )
-
-                results = {
-                    "following": analyze_following(user.id, list_id, api, cache),
-                    "followers": analyze_followers(user.id, api, cache),
-                    "timeline": analyze_timeline(
-                        user.id, list_id, api, cache
-                    ),
-                }
-                for key, value in results.items():
-                    if not value:
-                        raise Exception(
-                            f"Failed to fetch results for user {form.acct.data}."
-                        )
-            except Exception as exc:
-                import traceback
-
-                traceback.print_exc()
-                if isinstance(exc, MastodonNotFoundError):
-                    error = f"Could not find user {form.acct.data}."
+                if app.config["DRY_RUN"]:
+                    list_name = None
+                    following, followers, timeline = dry_run_analysis()
+                    results = {"following": following, "followers": followers, "timeline": timeline}
                 else:
-                    error = exc
-                error
+                    if session.get("lists") and form.lst and form.lst.data != "none":
+                        list_id = int(form.lst.data)
+                        list_name = [
+                            list["name"] 
+                            for list in session["lists"] 
+                            if int(list["id"]) == list_id
+                        ][0]
 
+                    try:
+                        api = get_mastodon_api(
+                            tok, INSTANCE
+                        )
+                        cache = Cache()
+
+                        user = get_user_from_handle(form.analyze_acct.data, api)
+
+                        if different_user and user.indexable is False:
+                            raise Exception(
+                                f"User {form.analyze_acct.data} is not indexable.\n"
+                                f"If the account is yours, you can change the setting on: "
+                                f"Settings > Public profile > Privacy and reach > "
+                                f"Include public posts in search results"
+                            )
+
+                        results = {
+                            "following": analyze_following(user.id, list_id, api, cache),
+                            "followers": analyze_followers(user.id, api, cache),
+                            "timeline": analyze_timeline(
+                                user.id, list_id, api, cache
+                            ),
+                        }
+                        for key, value in results.items():
+                            if not value:
+                                raise Exception(
+                                    f"Failed to fetch results "
+                                    f"for user {form.analyze_acct.data}."
+                                )
+                    except Exception as exc:
+                        import traceback
+
+                        traceback.print_exc()
+                        if isinstance(exc, MastodonNotFoundError):
+                            error = f"Could not find user {form.analyze_acct.data}."
+                        else:
+                            error = exc
+
+                    if error is not None:
+                        error = str(error).replace("\n", "<br>")
+
+            return render_template(
+                "index.html",
+                form=form,
+                results=results,
+                error=error,
+                div=div,
+                list_name=list_name,
+                TRACKING_ID=TRACKING_ID,
+            )
     return render_template(
         "index.html",
         form=form,
-        results=results,
-        error=str(error).replace("\n", "<br>"),
         div=div,
-        list_name=list_name,
         TRACKING_ID=TRACKING_ID,
     )
 
